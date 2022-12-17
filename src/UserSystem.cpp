6b322dede7f180f7d15e48af2493c3b14b2f6dbf
PolarisDane <2488721971@qq.com>
2022-12-17 16:46:28 +0800

    修改了.gitignore文件 初步完成了存储结构的模板化并封装为了头文件 初步完成了CMakeLists.txt 完成了UserSystem的架构(未运行检查)

--- src/UserSystem.cpp
+++ src/UserSystem.cpp
@@ -1,33 +1,61 @@
 #include "UserSystem.h"
 
-UserSystem::UserSystem() {
+UserSystem::UserSystem() :UserData("User") {
   BookstoreUser root("", "sjtu", "root", 7);
+  UserData.insert(element<BookstoreUser>{ "root", root });
 }
 
 UserSystem::~UserSystem() {
-
+  UserStack.clear();
 }
 
-UserSystem::UserRegister() {
-
+void UserSystem::UserRegister(const std::string& id, const std::string& password, const std::string& name, const int& p) {
+  BookstoreUser cur(id, password, name, p);
+  UserData.insert(element<BookstoreUser>{name, cur});
 }
 
-UserSystem::UserLogin() {
-
+void UserSystem::UserLogin(const std::string& name,const std::string& password) {
+  auto res = UserData.find(name);
+  if (res[0].val.user_password == password) UserStack.push_back(res[0].val);
+  else std::cerr << "wrong password" << std::endl;
 }
 
-UserSystem::ModifyPassword() {
-
+void UserSystem::ModifyPassword(const std::string& name,const std::string& curPassword,const std::string& newPassword) {
+  UserData.modify(name, curPassword, newPassword);
 }
 
-UserSystem::UserLogout() {
-
+void UserSystem::UserLogout(const std::string& name) {
+  if (UserStack.back().user_name != name) std::cerr << "undefined behaviour" << std::endl;
+  UserStack.back().choice = "";
+  UserStack.pop_back();
 }
 
 BookstoreUser::BookstoreUser() {
-
+  user_id = user_password = user_name = choice = "";
+  privilege = 0;
 }
 
-BookstoreUser::~BookstoreUser() {
+BookstoreUser::BookstoreUser(const std::string& id, const std::string& password, const std::string& name, const int& p) {
+  user_id = id; user_password = password; user_name = name;
+  privilege = p; choice = "";
+}
 
+bool BookstoreUser::operator<(const BookstoreUser& a)const {
+  return user_name < a.user_name;
+}
+
+bool BookstoreUser::operator>(const BookstoreUser& a)const {
+  return user_name > a.user_name;
+}
+
+bool BookstoreUser::operator<=(const BookstoreUser& a)const {
+  return user_name <= a.user_name;
+}
+
+bool BookstoreUser::operator>=(const BookstoreUser& a)const {
+  return user_name >= a.user_name;
+}
+
+bool BookstoreUser::operator==(const BookstoreUser& a)const {
+  return user_name == a.user_name;
 }
--- src/UserSystem.h
+++ src/UserSystem.h
@@ -4,32 +4,42 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
-
-class UserSystem {
-public:
-  UserSystem();
-  ~UserSystem();
-
-  void UserRegister();
-  void UserLogin();
-  void ModifyPassword();
-  void UserLogout();
-private:
-  BookstoreUser* currentUser;
-  std::vector<BookstoreUser*> UserStack;
-};
+#include "UnrolledLinkedList.h"
 
 class BookstoreUser {
+  friend class UserSystem;
+  friend class LinkList<BookstoreUser>;
 public:
   BookstoreUser();
-  ~BookstoreUser();
+  BookstoreUser(const std::string& id, const std::string& password, const std::string& name, const int& p);
+  ~BookstoreUser() = default;
+
+  bool operator <(const BookstoreUser& a)const;
+  bool operator >(const BookstoreUser& a)const;
+  bool operator <=(const BookstoreUser& a)const;
+  bool operator >=(const BookstoreUser& a)const;
+  bool operator ==(const BookstoreUser& a)const;
 
 private:
   std::string user_id;
   std::string user_password;
   std::string user_name;
   int privilege;
+  std::string choice;//appointed using ISBN(at least for now)
+};
 
+class UserSystem {
+public:
+  UserSystem();
+  ~UserSystem();
+
+  void UserRegister(const std::string& id, const std::string& password, const std::string& name, const int& p);
+  void UserLogin(const std::string& name, const std::string& password);
+  void ModifyPassword(const std::string& name,const std::string& curPassword,const std::string& newPassword);
+  void UserLogout(const std::string& name);
+private:
+  LinkList<BookstoreUser> UserData;
+  std::vector<BookstoreUser> UserStack;
 };
 
 #endif
